0563cb5284ea3d40e2a32abe31d719c17aabee5d
Ted Collins <theodore.collins2023@outlook.com>
2024-05-23 14:59:18 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ChallengeLabsWeek3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userString = GetStringFromUser();            
+            double total = AddIntegerTotal(userString);
+            Console.WriteLine(total);
+        }
+
+        static string GetStringFromUser()
+        {
+            string userInput ="";
+
+            do
+            {
+                Console.WriteLine("Please enter some text with both numbers and letters.");
+                userInput = Console.ReadLine();
+            }while (userInput == "");
+            
+            return userInput;
+        }
+
+        static double AddIntegerTotal(string nonsense)
+        {
+            
+            double sum = 0;
+            
+            for (int i = 0; i < nonsense.Length; i++)
+            {
+                if(char.IsNumber(nonsense[i]))
+                {
+                    sum = sum + char.GetNumericValue(nonsense,i);
+                }
+            }
+            return sum;
+        }
+    }
+}
